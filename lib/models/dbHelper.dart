b35488defec6f12e21534da31c2393764d4e1b3e
Abdelrahman <abdo10002011@gmail.com>
2021-07-24 08:36:16 +0200

    commit 5

--- lib/models/dbHelper.dart
+++ lib/models/dbHelper.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'product_item.dart';
@@ -8,7 +10,8 @@ class DbHelper {
   DbHelper.internal();
   late Database _db;
   _createDatabase() async {
-    if (_db != null) return _db;
+   // if (_db!= (null)) return _db;
+   // if (join(await getDatabasesPath(), 'q.db').isNotEmpty) return _db;
 
     String path = join(await getDatabasesPath(), 'q.db');
     _db = await openDatabase(path, version: 1,
@@ -29,19 +32,19 @@ class DbHelper {
   }
 
   addItem(ProductItem productItem) async {
-    Database db = await _createDatabase();
-    return db.insert('q1', productItem.toMaps());
+    Database? db = await _createDatabase();
+    return db!.insert('q1', productItem.toMaps());
   }
 
   allDateItem() async {
-    Database db = await _createDatabase();
-    db.query('q1');
+    Database? db = await _createDatabase();
+    db!.query('q1');
     return db.query('q1');
   }
 
   countItem() async {
-    Database db = await _createDatabase();
-    db.query('q1').then((value) {
+    Database? db = await _createDatabase();
+    db!.query('q1').then((value) {
       print(value.length);
       return value.length;
     });
